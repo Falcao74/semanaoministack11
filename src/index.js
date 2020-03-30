4aae0b15826e58acbb62e5cf4b0068553f845941
Guaraci Falcão <53480221+Falcao74@users.noreply.github.com>
2020-03-30 08:51:18 -0300

    segundo commit
    teste

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-const express = require("express"); //importando express
-const routes = require('./routes'); //importando as rotas
-const app = express(); //instanciando a aplicação 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-app.use(express.json());
-app.use(routes);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-app.listen(3333); //porta da aplicação
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
